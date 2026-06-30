889dc750a6ecd1c371c9f69b8dedd5c568b4fae4
shirinyannver31-ux <shirinyannver31@gmail.com>
2026-06-30 11:36:24 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+class Config:
+    ORIGINAL_GIST_LINK = "https://gist.githubusercontent.com/shirinyannver31-ux/6b16a88d07db0830b49ab8b02536c3b6/raw/0c3e1c08890073b7cee82b4b4c03b7b77237eb18/VedaVPN.txt"
